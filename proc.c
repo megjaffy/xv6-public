640833c5ba781049f06548d248eaedfbe84046b7
Tom Bressoud <tcbressoud@gmail.com>
2020-09-26 19:42:51 -0400

    create test program

--- proc.c
+++ proc.c
@@ -355,7 +355,7 @@ scheduler(void)
       // Switch to chosen process.  It is the process's job
       // to release ptable.lock and then reacquire it
       // before jumping back to us.
-      cprintf("SCHEDULED: %s\n", p->name);
+      cprintf("SCHEDULED: %s, %d\n", p->name, totalTickets);
       c->proc = p;
       switchuvm(p);
       p->state = RUNNING;
